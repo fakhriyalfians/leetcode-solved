071979555ded1f571c878cb8c08bf11779f508a4
fakhriyalfians <116850826+fakhriyalfians@users.noreply.github.com>
2024-05-01 17:55:49 +0700

    Time: 1162 ms (80.49%), Space: 0B (100.00%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,6 @@
+SELECT s.user_id,
+    ROUND(SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(s.user_id), 2) AS confirmation_rate
+FROM signups AS s
+LEFT JOIN confirmations AS c
+ON s.user_id = c.user_id
+GROUP BY 1
